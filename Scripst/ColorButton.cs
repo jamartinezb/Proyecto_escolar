22aaec7223777087e5febfda3f85766174449aca
Jhon Martinez B <38171433+jamartinezb@users.noreply.github.com>
2021-12-16 15:05:58 -0500

    Add files via upload

---
+++ Scripst/ColorButton.cs
@@ -0,0 +1,21 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButton : MonoBehaviour
+{
+    private Button _colorBtn;
+    private Image _colorImg;
+
+    private void Awake()
+    {
+        _colorBtn = GetComponent<Button>();
+        _colorImg = GetComponent<Image>();
+    }
+    private void Start() 
+    {
+        _colorBtn.onClick.AddListener(() => GameManager.Instance.ChangeColor(_colorImg.color));
+    
+    }
+}
---
+++ Scripst/FocusMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class FocusMode : MonoBehaviour {
+
+	//Hace foco automaticamente, generalmente el mas usado
+	public void modoContinuousAuto () {
+		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+
+
+}
